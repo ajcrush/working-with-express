f6b5808c513d0cd48124d646ecafa061e4a72b5f
Mohit sharma <mohitanand8987@gmail.com>
2024-08-25 13:07:23 +0530

    Completion of this module

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const http = require("http");
+const bodyParser = require("body-parser");
 const express = require("express");
-
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const path = require("path");
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 app.use((req, res, next) => {
-  console.log("In the middleware");
-  next(); // Allow the request to continue to the nec=xt middleware lines
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
-app.use((req, res, next) => {
-  console.log("In the another middleware");
-});
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
